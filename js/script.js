3a444a3783d5408c21cc3168804ceb60e3678188
h0ler <63481484+Holder-hash@users.noreply.github.com>
2022-12-31 02:04:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+firstSlide.onclick = () => {
+    window.open('https://steamcommunity.com/sharedfiles/filedetails/?id=2688341058');
+};
+
+secondSlide.onclick = () => {
+    window.open('https://steamcommunity.com/sharedfiles/filedetails/?id=2697815680');
+};
+
+thirdSlide.onclick = () => {
+    window.open('https://steamcommunity.com/sharedfiles/filedetails/?id=2690118884');
+};
+
